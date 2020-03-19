7d07ebfa408775ff94aaaf2fd10836b105f8519c
mmgertrude <22059601+mmgertrude@users.noreply.github.com>
2020-03-19 15:22:27 +0100

    ere

--- webslideData/Models/ApplicationDbContext.cs
+++ webslideData/Models/ApplicationDbContext.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace webslideData.Models
 {
-    class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+
+        public DbSet<Tutorial> Tutorials { get; set; }
+      
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Tutorial>().ToTable("Tutorials");
+            
+        }
     }
 }
