710de218ff95ff0a4dd5ccd5e0154b4ca1f9eeaf
mmgertrude <22059601+mmgertrude@users.noreply.github.com>
2020-03-19 15:06:30 +0100

    trt

---
+++ webslideData/Models/Tutorial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace webslideData.Models
+{
+   public class Tutorial
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public DateTime StartDate { get; set; }
+        public int Atttendees { get; set; }
+        public string Actions { get; set; }
+    }
+}
